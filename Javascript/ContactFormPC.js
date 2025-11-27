9cef7ee35a8679af1cccd6c4caf4cde83f3f9353
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2025-11-26 23:28:08 -0500

    Update PC builds HTML and image paths

--- Javascript/ContactFormPC.js
+++ Javascript/ContactFormPC.js
@@ -6,42 +6,42 @@ const PCBuildsHTML = `
       <div class="build empty-slot"></div>
 
       <div class="build fade-in" onclick="selectPC('Gaming Beast','Ryzen 7 7800X3D, RTX 4070 Ti, 32GB RAM, 1TB NVMe','$3,499')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/highendpc.png" alt="Gaming PC Build">
+        <img src="/images/highendpc.png" alt="Gaming PC Build">
         <h3>Gaming Beast</h3>
         <p>Ryzen 7 7800X3D, RTX 4070 Ti, 32GB RAM, 1TB NVMe</p>
         <p class="price">$3,499</p>
       </div>
 
       <div class="build fade-in" onclick="selectPC('Gaming Build','Intel I7 10700k, Radeon Strix RX 6700 XT, 16GB RAM, NVMe 1TB','$1,299')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/gamingpc.png" alt="Gaming Build">
+        <img src="/images/gamingpc.png" alt="Gaming Build">
         <h3>Gaming Build</h3>
         <p>Intel I7 10700k, Radeon Strix RX 6700 XT, 16GB RAM, NVMe 1TB</p>
         <p class="price">$1,299</p>
       </div>
 
       <div class="build fade-in" onclick="selectPC('Budget Starter','Ryzen 7 5800, GTX 1660 Super, 16GB RAM, NVMe 500GB','$999')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/budgetfriendly.png" alt="Budget Starter">
+        <img src="/images/budgetfriendly.png" alt="Budget Starter">
         <h3>Budget Starter</h3>
         <p>Ryzen 7 5800, GTX 1660 Super, 16GB RAM, NVMe 500GB</p>
         <p class="price">$999</p>
       </div>
 
       <div class="build fade-in" onclick="selectPC('Budget Starter (AIO)','Ryzen 7 5700G, Radeon Vega 8, 16GB RAM, NVMe 500GB','$849')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/Cool-Pc-Setup.png" alt="Budget Starter">
+        <img src="/images/Cool-Pc-Setup.png" alt="Budget Starter">
         <h3>Budget Starter</h3>
         <p>Ryzen 7 5700G, Radeon Vega 8, 16GB RAM, NVMe 500GB</p>
         <p class="price">$849</p>
       </div>
 
       <div class="build fade-in" onclick="selectPC('Budget PC Build','Ryzen 5 5600G, Radeon Vega 7, 16GB RAM, NVMe 500GB','$699')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/White-Budget-Gaming-PC.png" alt="Budget PC Build">
+        <img src="/images/White-Budget-Gaming-PC.png" alt="Budget PC Build">
         <h3>Budget PC Build</h3>
         <p>Ryzen 5 5600G, Radeon Vega 7, 16GB RAM, NVMe 500GB</p>
         <p class="price">$699</p>
       </div>
 
       <div class="build fade-in" onclick="selectPC('Custom Build','N/A, N/A, N/A, N/A','$200')">
-        <img src="https://screwsandcircuits.com/wp-content/uploads/2025/03/pcparts.png" alt="Custom Build">
+        <img src="/images/pcparts.png" alt="Custom Build">
         <h3>Custom built to your liking</h3>
         <p>N/A, N/A, N/A, N/A</p>
         <p class="price">$200</p>
