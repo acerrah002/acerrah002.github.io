da782751f1fdffa384e887188fce31289cf85ef2
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2025-11-30 21:01:39 -0500

    updated the site and removed bugs
    I made the hamburger UI actually be on the very right and fixed the buttons not taking up all the sidebar space

--- Javascript/header.js
+++ Javascript/header.js
@@ -1,23 +1,23 @@
 const HeaderHTML = `
   <img src="/images/logo.png" alt="ScrewsandCircuits Logo" id="logo">
          <div>
-            <nav>
-              <ul class="sidebar">
+          <nav>
+            <ul class="sidebar">
                 <li onclick=hideSidebar()><a href="#"><p id="X">X</p></a></li>
-                <li><a href="index.html" target="_blank">Home Page</a></li>
-                <li><a href="fobkeys.html" >Fob keys</a></li>
-                <li><a href="maintenance.html" >Maintenance & Repair</a></li>
-                <li><a href="3DPrinting.html" >3DPrinting</a></li>
-                <li><a href="Pc_builds.html">Pc Builds</a></li>
-                <li><a href="Tutorials.html">Tutorials</a></li>
+                <li onclick="hideSidebar()"><a href="index.html">Home Page</a></li>
+                <li onclick=hideSidebar()><a href="fobkeys.html" >Fob keys</a></li>
+                <li onclick=hideSidebar()><a href="maintenance.html" >Maintenance & Repair</a></li>
+                <li onclick=hideSidebar()><a href="3DPrinting.html" >3DPrinting</a></li>
+                <li onclick=hideSidebar()><a href="Pc_builds.html">Pc Builds</a></li>
+                <li onclick=hideSidebar()><a href="Tutorials.html">Tutorials</a></li>
                 <li><a href="https://www.facebook.com/p/ScrewsCircuits-61577351262288/" target="_blank">Facebook</a></li>
                 <li><a href="https://www.instagram.com/screwsandcircuits/?hl=en"  target="_blank">Instagram</a></li>
-                <li><a href="#contact">Contact Us</a></li>
+                <li onclick=hideSidebar()><a href="#contact">Contact Us</a></li>
               </ul>
               <ul>
-                <li class="navbarlinks"><a href="index.html">Home</a></li>
-                <li class="navbarlinks"><a href="#Services">Services</a></li>
-                <li class="navbarlinks"><a href="#contact">Contact</a></li>
+                <li class="navbarlinks" onclick=hideSidebar()><a href="index.html">Home</a></li>
+                <li class="navbarlinks" onclick=hideSidebar()><a href="#Services">Services</a></li>
+                <li class="navbarlinks" onclick=hideSidebar()><a href="#contact">Contact</a></li>
                 <li onclick=showSidebar() ><a href="#" ><div class="hamburger"><span></span><span></span><span></span></div></a></li>
               </ul>
             </nav>
@@ -31,6 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
     return;
   }
   contentArea.innerHTML = HeaderHTML;
+
+const xButton = document.querySelector('#X');
+   if (xButton) {
+      xButton.parentNode.addEventListener('click', function(e) {
+      e.preventDefault(); 
+      e.stopPropagation();
+     hideSidebar();
+  });
+  }
 });
 
 let sidebarVisible = false;
@@ -62,10 +71,6 @@ document.addEventListener('click', function(event) {
     }
 });
 
-// Stop propagation on X button click to prevent double-trigger
-document.querySelector('#X').addEventListener('click', function(e) {
-    e.stopPropagation();
 
-});
 
 
